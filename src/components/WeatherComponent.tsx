0ae830ec0ce28346567c0e23cbaec0d0d2473e80
zakarpatrik <patrik.zakar@garanddesign.hu>
2024-03-10 14:48:27 +0100

    created useCity hook to get city name

--- src/components/WeatherComponent.tsx
+++ src/components/WeatherComponent.tsx
@@ -1,9 +1,11 @@
 'use client';
+import useCity from '@/hooks/useCity';
 import useWeather from '@/hooks/useWeather';
 import React from 'react';
 
 const WeatherComponent = () => {
   const weatherInfo = useWeather({ latitude: 47.498, longitude: 19.0399 });
+  const city = useCity({ latitude: 47.498, longitude: 19.0399 });
   return (
     <>
       <div>{weatherInfo.data?.main}</div>
---
+++ src/hooks/useCity.ts
@@ -0,0 +1,49 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+interface CityData {
+  city: string;
+}
+
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+const useCity = ({ latitude, longitude }: Coordinates) => {
+  const [data, setData] = useState<CityData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const apiKey = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY;
+        const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=5&appid=${apiKey}`;
+
+        const response = await fetch(url);
+        const result = await response.json();
+
+        if (response.ok) {
+          setData({
+            city: result.name,
+          });
+        } else {
+          throw new Error(result.message);
+        }
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [latitude, longitude]);
+
+  return { data, loading, error };
+};
+
+export default useCity;
